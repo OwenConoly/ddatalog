c7dc4b1033f6e23253724f0493fdcb2e49fc1b9a
tnv5 <tnv5@cornell.edu>
2025-10-24 11:35:29 -0400

    added basic program

---
+++ .Makefile.coq.d
@@ -0,0 +1,2 @@
+src/DatalogProgram.vo src/DatalogProgram.glob src/DatalogProgram.v.beautified src/DatalogProgram.required_vo: src/DatalogProgram.v datalog/src/Datalog.vo /Users/tia/.opam/frap/lib/rocq-runtime/rocqworker
+src/DatalogProgram.vos src/DatalogProgram.vok src/DatalogProgram.required_vos: src/DatalogProgram.v datalog/src/Datalog.vos /Users/tia/.opam/frap/lib/rocq-runtime/rocqworker
