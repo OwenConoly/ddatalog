bc518c85d55112660bac72d1c58c4ebfc7b090a1
Owen Conoly <otconoly@hotmail.com>
2025-12-15 11:50:46 -0500

    switch datalog submodule to agg2 branch

--- .Makefile.coq.d
+++
@@ -1,2 +0,0 @@
-src/DatalogProgram.vo src/DatalogProgram.glob src/DatalogProgram.v.beautified src/DatalogProgram.required_vo: src/DatalogProgram.v datalog/src/Datalog.vo /Users/tia/.opam/frap/lib/rocq-runtime/rocqworker
-src/DatalogProgram.vos src/DatalogProgram.vok src/DatalogProgram.required_vos: src/DatalogProgram.v datalog/src/Datalog.vos /Users/tia/.opam/frap/lib/rocq-runtime/rocqworker
