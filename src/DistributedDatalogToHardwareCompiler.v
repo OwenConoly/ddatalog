e8c35e148d21cc2f48ab3b40836cd2d467fbdb63
Owen Conoly <otconoly@hotmail.com>
2026-08-10 16:59:05 -0400

    get rid of redundant check in ftables_in_graphb

--- src/DistributedDatalogToHardwareCompiler.v
+++ src/DistributedDatalogToHardwareCompiler.v
@@ -494,11 +494,8 @@ Definition hops_in_graphb (g : node_graph) (n : node_id) (hops : list node_id) :
 Definition ftable_in_graphb (g : node_graph) (n : node_id) (ft : internode_forwarding_table) :=
   map.forallb (fun _ hops => hops_in_graphb g n hops) ft.
 
-(*TODO get rid of redundant check_node_valid thing*)
 Definition ftables_in_graphb (g : node_graph) (ftables : internode_forwarding_tables) : bool :=
-  map.forallb
-    (fun n ft => check_node_valid n (ComputableGraph.nodes g) && ftable_in_graphb g n ft)
-    ftables.
+  map.forallb (ftable_in_graphb g) ftables.
 
 Definition edge_ftables (ftables : internode_forwarding_tables) : node_ftable_map :=
   map.map_values (map.map_values (List.map DestEdge)) ftables.
